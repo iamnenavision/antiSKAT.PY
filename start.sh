23f945c0847aef57cb34b98fa72556f07f477e8b
kappaprideonly <kolmanoney@gmail.com>
2022-04-03 13:19:59 +0300

    shablon

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo -e "Введите API-KEY-TG!"
 read KEY
 echo -e $KEY
 sudo docker build -t bot --build-arg KEY_TG=$KEY .
-sudo docker run --rm -e KEY_TG=$KEY bot
+sudo docker run --name=bot --rm -e KEY_TG=$KEY bot
